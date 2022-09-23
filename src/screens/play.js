4d9ca3d16a1d864ca7590b554bc776d8b216f2f0
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-09-23 10:53:13 +0530

    play screen

---
+++ src/screens/play.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
+import {useNavigation} from '@react-navigation/core';
+
+const Play = () => {
+
+  const navigation = useNavigation();
+
+  return (
+    <SafeAreaView>
+        <ScrollView>
+            <View>
+
+            </View>
+        </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default Play;
