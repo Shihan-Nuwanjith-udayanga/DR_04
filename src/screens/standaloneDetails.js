1820f10d012101aa26915b81fe86e0491bd2a593
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-10-03 22:44:47 +0530

    change standalone Details

--- src/screens/standaloneDetails.js
+++ src/screens/standaloneDetails.js
@@ -17,58 +17,105 @@ const StandaloneDetails = () => {
 
   return (
     <SafeAreaView>
-        <ScrollView>
-            <View style={styles.headerView}>
-              <TouchableOpacity style={styles.imageTouchable} onPress={()=> navigation.navigate("Home")}>
-                <Image source={require('../assets/images/back.png')} style={styles.image} />
-              </TouchableOpacity>
-              <TouchableOpacity style={styles.imageTouchable} onPress={()=> navigation.navigate("Home")}>
-                <Image source={require('../assets/images/home.png')} style={styles.image1} />
-              </TouchableOpacity>
-            </View>
-            <View style={styles.container}>
-              <Image source={require('../assets/images/standalone.png')} style={styles.logoImage} />
-              <Text style={styles.text}>Standalone Application Development</Text>
-            </View>
-            <ScrollView style={styles.scrollView}>
-            <View style={styles.categoryView}>
-              <TouchableOpacity style={styles.categoryTouchable} onPress={() => Linking.openURL('https://www.java.com/en/')}>
-                <Image source={require('../assets/images/java.png')} style={styles.categoryImage} />
-                <Text style={styles.txtCategory}>Java</Text>
-              </TouchableOpacity>
-            </View>
-            <View style={styles.categoryView}>
-              <TouchableOpacity style={styles.categoryTouchable} onPress={() => Linking.openURL('https://www.python.org/')}>
-                <Image source={require('../assets/images/python.png')} style={styles.categoryImage} />
-                <Text style={styles.txtCategory}>Python</Text>
-              </TouchableOpacity>
-            </View>
-            <View style={styles.categoryView}>
-              <TouchableOpacity style={styles.categoryTouchable} onPress={() => Linking.openURL('https://www.mysql.com/')}>
-                <Image source={require('../assets/images/mysql.png')} style={styles.categoryImage} />
-                <Text style={styles.txtCategory}>MySQL</Text>
-              </TouchableOpacity>
-            </View>
-            <View style={styles.categoryView}>
-              <TouchableOpacity style={styles.categoryTouchable} onPress={() => Linking.openURL('https://www.w3schools.com/cs/index.php')}>
-                <Image source={require('../assets/images/cSharp.png')} style={styles.categoryImage} />
-                <Text style={styles.txtCategory}>C#</Text>
-              </TouchableOpacity>
-            </View>
-            <View style={styles.categoryView}>
-              <TouchableOpacity style={styles.categoryTouchable} onPress={() => Linking.openURL('https://www.w3schools.com/cpp/')}>
-                <Image source={require('../assets/images/cPlus.png')} style={styles.categoryImage} />
-                <Text style={styles.txtCategory}>C++</Text>
-              </TouchableOpacity>
-            </View>
-            <View style={styles.categoryView}>
-              <TouchableOpacity style={styles.categoryTouchable} onPress={() => Linking.openURL('https://spring.io/projects/spring-boot/')}>
-                <Image source={require('../assets/images/spring.png')} style={styles.categoryImage} />
-                <Text style={styles.txtCategory}>Spring Boot</Text>
-              </TouchableOpacity>
-            </View>
-            </ScrollView>
+      <ScrollView>
+        <View style={styles.headerView}>
+          <TouchableOpacity
+            style={styles.imageTouchable}
+            onPress={() => navigation.navigate('Home')}>
+            <Image
+              source={require('../assets/images/back.png')}
+              style={styles.image}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.imageTouchable}
+            onPress={() => navigation.navigate('Home')}>
+            <Image
+              source={require('../assets/images/home.png')}
+              style={styles.image1}
+            />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.container}>
+          <Image
+            source={require('../assets/images/standalone.png')}
+            style={styles.logoImage}
+          />
+          <Text style={styles.text}>Standalone Application Development</Text>
+        </View>
+        <ScrollView style={styles.scrollView}>
+          <View style={styles.categoryView}>
+            <TouchableOpacity
+              style={styles.categoryTouchable}
+              onPress={() => Linking.openURL('https://www.java.com/en/')}>
+              <Image
+                source={require('../assets/images/java.png')}
+                style={styles.categoryImage}
+              />
+              <Text style={styles.txtCategory}>Java</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.categoryView}>
+            <TouchableOpacity
+              style={styles.categoryTouchable}
+              onPress={() => Linking.openURL('https://www.python.org/')}>
+              <Image
+                source={require('../assets/images/python.png')}
+                style={styles.categoryImage}
+              />
+              <Text style={styles.txtCategory}>Python</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.categoryView}>
+            <TouchableOpacity
+              style={styles.categoryTouchable}
+              onPress={() => Linking.openURL('https://www.mysql.com/')}>
+              <Image
+                source={require('../assets/images/mysql.png')}
+                style={styles.categoryImage}
+              />
+              <Text style={styles.txtCategory}>MySQL</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.categoryView}>
+            <TouchableOpacity
+              style={styles.categoryTouchable}
+              onPress={() => Linking.openURL('https://www.w3schools.com/cpp/')}>
+              <Image
+                source={require('../assets/images/cPlus.png')}
+                style={styles.categoryImage}
+              />
+              <Text style={styles.txtCategory}>C++</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.categoryView}>
+            <TouchableOpacity
+              style={styles.categoryTouchable}
+              onPress={() =>
+                Linking.openURL('https://www.w3schools.com/cs/index.php')
+              }>
+              <Image
+                source={require('../assets/images/cSharp.png')}
+                style={styles.categoryImage}
+              />
+              <Text style={styles.txtCategory}>C#</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.categoryView}>
+            <TouchableOpacity
+              style={styles.categoryTouchable}
+              onPress={() =>
+                Linking.openURL('https://spring.io/projects/spring-boot/')
+              }>
+              <Image
+                source={require('../assets/images/spring.png')}
+                style={styles.categoryImage}
+              />
+              <Text style={styles.txtCategory}>Spring Boot</Text>
+            </TouchableOpacity>
+          </View>
         </ScrollView>
+      </ScrollView>
     </SafeAreaView>
   );
 };
